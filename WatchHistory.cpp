ba5432c4cfcae3d7ae0c55ee132d5f777383d417
surlahlee <tovvards@gmail.com>
2024-12-12 23:17:30 +0900

    Update WatchHistory.cpp

--- WatchHistory.cpp
+++ WatchHistory.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <stdexcept>
 
+// addMovieToHistory 함수: 영화 시청 기록을 history 벡터에 추가하는 함수
 void WatchHistory::addMovieToHistory(const std::string& movieTitle, const std::string& date, float rating) {
     history.push_back({movieTitle, date, rating});
 }
@@ -12,7 +13,7 @@ void WatchHistory::saveHistoryToFile() {
     if (!file) {
         throw std::runtime_error("Error opening history file for writing.");
     }
-
+// history 벡터에 저장된 각 시청 기록을 파일에 저장
     for (const auto& record : history) {
         file << record.movieTitle << "," << record.date << "," << record.rating << std::endl;
     }
