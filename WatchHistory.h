e3ebf31a7ab7fe2db183c70da1ab46bc99503d18
surlahlee <tovvards@gmail.com>
2024-12-12 23:18:42 +0900

    Update WatchHistory.h

--- WatchHistory.h
+++ WatchHistory.h
@@ -4,12 +4,14 @@
 #include <string>
 #include <fstream>
 
+// WatchHistory 클래스: 영화 시청 기록을 관리하고, 이를 파일에 저장하는 클래스
 class WatchHistory {
 public:
+// addMovieToHistory 함수: 새로운 시청 기록을 history 벡터에 추가하는 함수
     void addMovieToHistory(const std::string& movieTitle, const std::string& date, float rating) {
         history.push_back({movieTitle, date, rating});
     }
-
+// saveHistoryToFile 함수: 시청 기록을 "watch_history.txt" 파일에 저장하는 함수
     void saveHistoryToFile() {
         std::ofstream file("watch_history.txt");
         if (!file) {
@@ -23,6 +25,7 @@ public:
     }
 
 private:
+// WatchRecord 구조체: 영화 시청 기록을 저장하는 구조체
     struct WatchRecord {
         std::string movieTitle;
         std::string date;
