917b1e79c259c7808eb944b87ba7432299676efe
surlahlee <tovvards@gmail.com>
2024-12-12 23:10:56 +0900

    Update FileManager.cpp

--- FileManager.cpp
+++ FileManager.cpp
@@ -4,13 +4,17 @@
 #include <sstream>
 #include <stdexcept>
 
+// 영화 데이터를 파일에서 읽어와 'movies' 벡터에 추가하는 함수
 void FileManager::loadMoviesFromFile(std::vector<Movie*>& movies) {
+    // 파일을 읽기 모드로 열기
     std::ifstream file("movies.txt");
+    // 파일 열기 실패 시 예외를 던짐
     if (!file) {
         throw std::runtime_error("Error opening movie file for reading.");
     }
 
     std::string line;
+    // 파일에서 한 줄씩 읽어옴
     while (std::getline(file, line)) {
         std::stringstream ss(line);
         std::string title, genre;
@@ -18,11 +22,12 @@ void FileManager::loadMoviesFromFile(std::vector<Movie*>& movies) {
         std::getline(ss, title, ',');
         std::getline(ss, genre, ',');
         ss >> rating;
+        // 새 GenreMovie 객체를 생성하고, movies 벡터에 추가
         movies.push_back(new GenreMovie(title, genre, rating));
     }
     file.close();
 }
-
+// 영화 데이터를 파일에 저장하는 함수
 void FileManager::saveMoviesToFile(const std::vector<Movie*>& movies) {
     std::ofstream file("movies.txt");
     if (!file) {
