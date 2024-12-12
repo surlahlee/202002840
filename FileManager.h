5967b659b59e1087a222eed91d41a52405a17820
surlahlee <tovvards@gmail.com>
2024-12-12 23:12:15 +0900

    Update FileManager.h

--- FileManager.h
+++ FileManager.h
@@ -6,6 +6,7 @@
 #include "Movie.h"
 #include "GenreMovie.h"
 
+// 파일 읽기 및 저장을 담당하는 FileManager 클래스
 class FileManager {
 public:
     static void loadMoviesFromFile(std::vector<Movie*>& movies) {
@@ -26,13 +27,13 @@ public:
         }
         file.close();
     }
-
+// 영화 데이터를 'movies' 벡터에서 읽어 "movies.txt" 파일에 저장하는 정적 함수
     static void saveMoviesToFile(const std::vector<Movie*>& movies) {
         std::ofstream file("movies.txt");
         if (!file) {
             throw std::runtime_error("Error opening movie file for writing.");
         }
-
+ // movies 벡터에 있는 각 영화 객체의 정보를 파일에 저장
         for (const auto& movie : movies) {
             file << movie->getTitle() << "," << movie->getGenre() << "," << movie->getRating() << std::endl;
         }
